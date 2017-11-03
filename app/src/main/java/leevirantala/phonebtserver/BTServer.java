75080f512b764e673a0dcb938e951bca2551574f
Leevi Rantala <leevirantala@gmail.com>
2017-11-03 09:54:00 +0200

    Works once, then crashes to socket closed exception.

--- app/src/main/java/leevirantala/phonebtserver/BTServer.java
+++ app/src/main/java/leevirantala/phonebtserver/BTServer.java
@@ -4,12 +4,16 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.UUID;
 
@@ -17,52 +21,84 @@ import java.util.UUID;
  * Created by eewijet on 31.10.2017.
  */
 
-public class BTServer implements Runnable {
+public class BTServer extends Thread implements Runnable {
     private static BluetoothAdapter bluetoothAdapter;
-    private boolean running = true;
+    private BTConnectionReader btConnectionReader = null;
     
     private final UUID uuid = UUID.fromString("ee2133aa-b8a2-11e7-abc4-cec278b6b50a");
     private static String TAG = BTServer.class.getSimpleName();
     
-    private BluetoothServerSocket serverSocket;
-    private BluetoothSocket socket;
-    private DataInputStream dataInputStream;
-    private OutputStream outputStream;
-    private InputStream inputStream;
-    private byte[] buffer = new byte[1024];
-    private int bytesRead;
-    private long fileSize;
-    private ByteArrayOutputStream readResult;
-    private int streamLength;
-    private boolean reading;
-    
-    
-    @Override
-    public void run() {
-        try {
-            serverSocket = bluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord("PulseServer", uuid);
-            socket = serverSocket.accept();
-            dataInputStream = (DataInputStream) socket.getInputStream();
-            String fileName = dataInputStream.readUTF();
-            fileSize = dataInputStream.readLong();
-            outputStream = new FileOutputStream(fileName);
-            while (fileSize > 0 && (bytesRead = dataInputStream.read(buffer, 0, (int) Math.min(buffer.length, fileSize))) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-                fileSize -= bytesRead;
+    private class BTConnectionReader implements Runnable {
+        private BluetoothServerSocket serverSocket;
+        private BluetoothSocket clientSocket;
+        private DataInputStream dataInputStream;
+        private OutputStream outputStream;
+        private InputStream inputStream;
+        private BufferedReader bufferedReader;
+        private byte[] buffer = new byte[1024];
+        private int bytesRead;
+        private long fileSize;
+        private ByteArrayOutputStream readResult;
+        private int streamLength;
+        private boolean reading;
+        String message = "";
+        
+        @Override
+        public void run() {
+            try {
+                serverSocket = BluetoothAdapter.getDefaultAdapter().listenUsingRfcommWithServiceRecord("PulseServer", uuid);
+                clientSocket = serverSocket.accept();
+                serverSocket.close();
+                inputStream = clientSocket.getInputStream();
+                Log.d(TAG, "Run entered");
+                    //bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                while (reading){
+                    dataInputStream = new DataInputStream(inputStream);
+                    message = dataInputStream.readUTF();
+                    Log.d(TAG, message);
+                
+                    /*dataInputStream = (DataInputStream) socket.getInputStream();
+                    String fileName = dataInputStream.readUTF();
+                    fileSize = dataInputStream.readLong();
+                    outputStream = new FileOutputStream(fileName);
+                    while (fileSize > 0 && (bytesRead = dataInputStream.read(buffer, 0, (int) Math.min(buffer.length, fileSize))) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                        fileSize -= bytesRead;
+                    }*/
             }
-            outputStream.close();
-        } catch (IOException e) {
-            Log.e(TAG, "Thread IOException error", e);
+            } catch (IOException e) {
+                Log.d(TAG, "Thread IOException error", e);
+                e.printStackTrace();
+            }
+        }
+    
+    
+        public void startReading() {
+            Log.d(TAG, "startReading");
+            Log.d(TAG, "Starting server");
+            reading = true;
+            run();
+        }
+    
+        public void stopReading() {
+            Log.d(TAG, "stopReading");
+            Log.d(TAG, "Stopping server");
+            reading = false;
         }
     }
-    
-    public void startReading(){
-        Log.d(TAG, "startReading");
-        reading = true;
+
+    public void connect() {
+        //Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        //startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        //bluetoothAdapter.enable();
+        Log.d(TAG, "connect");
+        btConnectionReader = new BTConnectionReader();
+        btConnectionReader.startReading();
     }
-    
-    public void stopReading(){
-        Log.d(TAG, "stopReading");
-        reading = false;
+
+    public void disconnect() {
+        //bluetoothAdapter.disable();
+        Log.d(TAG, "disconnect");
+        btConnectionReader.stopReading();
     }
 }
--- app/src/main/java/leevirantala/phonebtserver/MainActivity.java
+++ app/src/main/java/leevirantala/phonebtserver/MainActivity.java
@@ -2,6 +2,7 @@ package leevirantala.phonebtserver;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,8 +10,10 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int DISCOVERABLE_REQUEST_CODE = 0x1;
     Button connectbutton, sendbutton;
     TextView display;
     final String TAG = MainActivity.class.getSimpleName();
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private StringBuffer mOutStringBuffer;
     private BluetoothAdapter btAdapter = null;
     private BluetoothManager btManager = null;
+    private BTServer btServer = null;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +45,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     
     @Override
     public void onClick(View v) {
+        Log.d(TAG, "onClick");
         switch(v.getId()){
             case R.id.connectbutton:
-                //Do something
+                if (btServer == null){
+                    Toast.makeText (this, "Making server, connecting", Toast.LENGTH_SHORT).show();
+                    /*if(btAdapter != null && btAdapter.isDiscovering()){
+                        btAdapter.cancelDiscovery();
+                    }
+                    btAdapter.startDiscovery();*/
+                    Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    startActivityForResult(discoverableIntent, DISCOVERABLE_REQUEST_CODE);
+                    btServer = new BTServer();
+                    btServer.connect();
+                }
+                else if (btServer != null){
+                    Toast.makeText(this, "Nulling server, disconnecting", Toast.LENGTH_SHORT).show();
+                    if(btAdapter != null && btAdapter.isDiscovering()){
+                        btAdapter.cancelDiscovery();
+                    }
+                    btServer.disconnect();
+                    btServer = null;
+                }
                 break;
             case R.id.sendbutton:
                 //Do something else
+                Toast.makeText(this, "Doesn't do anything yet", Toast.LENGTH_SHORT).show();
                 break;
         }
             
